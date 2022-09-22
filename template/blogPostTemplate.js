d2488171835afa77e7acb215e941357663bb6b0f
Alex P <Alex.peschel@gmail.com>
2022-09-22 17:50:20 +0200

    A template for the group to follow in the case where they want to create a blog post

---
+++ template/blogPostTemplate.js
@@ -0,0 +1,20 @@
+document.write(
+    '<div class="post">\
+        <div style="display:flex;">\
+            <h2> Created the base architecture </h2>\
+            <div style="margin-left: auto;">\
+                <h3> GameDev </h3>\
+                <h3> 21/9-22 </h3>\
+            </div>\
+        </div>\
+    \
+        <p>\
+            Here you can write your own text. However due to the format I want to inform you how to write in here properaly.\
+            As you can se each row needs to end with a back slash, i mean you can\'t even have spaces after the backslash.\
+            Then in the case where you want to write a function write it with qutastion marks such as " instead of the\
+            lonesume such as \' beacuse then it will fuck up. \
+            <br><br>\
+            As can be seen if you want to for exemple write don\'t then you will need a backslash before the backtick \
+        </p>\
+    </div>\
+    ');
