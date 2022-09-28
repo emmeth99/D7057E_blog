79ca56b4e66bc0170534dc3244c0c142bdd4beab
emmeth <fantaexotic1999@gmail.com>
2022-09-28 13:39:54 +0200

    fixed typo

--- frontEndBlog/28_09_22.js
+++ frontEndBlog/28_09_22.js
@@ -1,7 +1,7 @@
 document.write(
     '<div class="post">\
         <div style="display:flex;">\
-            <h2> Created the base architecture </h2>\
+            <h2> Designing and fluttering </h2>\
             <div style="margin-left: auto;">\
                 <h3> FrontEnd </h3>\
                 <h3> 28/9-22 </h3>\
@@ -10,15 +10,15 @@ document.write(
     \
         <p>\
             Since last post we front-enders have worked with the design of the startpage and the page with the game board.\
-            We have also tried to get flutter up and running on our computers which has proved to be a bit diffult on some computers.\
+            We have also tried to get flutter up and running which has proved to be a bit diffult on some computers.\
             Those of us with working flutter setups have started to learn and experiment with it \
             so that we can hopefully get a prototype up sometime in the close future :)\
             <br> <br>\
-            Here are our first draft of designs: \
-            <img src="frontEndBlog/images/design_till_startsidan.png" alt="First draft of design for frontpage" class="center">\
+            Here are our current designs: <br>\
+            <img src="frontEndBlog/images/design_till_startsidan.png" alt="First draft of design for frontpage" class="center"> <br>\
             <img src="frontEndBlog/images/Startdesign_av_spelsida.png" alt="First draft of design for page with game board" class="center">\
             <br> <br>\
-            And here is a prototype in progress made by Emil: \
+            And here is a prototype in progress made by Emil: <br>\
             <img src="frontEndBlog/images/prototyp1.png" alt="Prototyping in progress" class="center">\
         </p>\
     </div>\
