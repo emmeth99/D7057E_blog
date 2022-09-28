8940fb53c394c95b797564bf37f0dfe33a9d5c1c
Josef-Vilestad <josef.vilestad@gmail.com>
2022-09-28 14:10:43 +0200

    backend first post

---
+++ backendBlog/22-09-28.js
@@ -0,0 +1,17 @@
+document.write(
+    `<div class="post"> \
+          <div style='display:flex;'>\
+              <h2> VM is up </h2>\
+              <div style='margin-left: auto;'>\
+                <h3> Frontend </h3>\
+                <h3> 28/9-22 </h3>\
+              </div>\
+            </div>\
+          <p>\
+            After some problems we finally have set up a VM on Ludd with at public IP,\
+            the link for the backend server is <a href="http://130.240.202.95:7351"> http://130.240.202.95:7351</a> when it is on.\
+            <br>\
+            The next step is to develop a POC for a client to communicate with the server.\
+          </p>\
+      </div>\
+  </div>\`);
