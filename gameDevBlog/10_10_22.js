107c8edc862dbfefeff1d220bf1bb0edf1c7ae9f
Alex P <Alex.peschel@gmail.com>
2022-10-10 11:41:08 +0200

    add gamedev blog for 10/10-22

---
+++ gameDevBlog/10_10_22.js
@@ -0,0 +1,25 @@
+document.write(
+    '<div class="post">\
+      <div style="display:flex;">\
+        <h2> End of Sprint 1 </h2>\
+          <div style="margin-left: auto;">\
+          <h3> GameDev </h3>\
+          <h3> 21/9-22 </h3>\
+        </div>\
+      </div>\
+    \
+      <p>\
+      During sprint 1 the group has set up a working tetris game.\
+      A game where you can place, rotate, and move blocks while also being able to clear rows when blocks are placed.\
+      This is now visable due to the work of frontEnd, where they call the tick state within gamedev to do what is needed.\
+      <br><br>\
+      We have had some problems early on where we had nothing for the team mates to do. This was mainly done due to bad\
+      sprint 1 planing. The plan is to improve this on sprint 2 planing meating. Rather create more issues then less.\
+      The we have also found a couple of bugs during the game play. However, a large portion of them has been solved \
+      during the process. Multiple rows deleted when only one should have been deleted.\
+      <br><br>\
+      So after sprint 1, we will take the things we have learned and improve for sprint 2. Continue to suport the team \
+      if someone needs help. Then continue to improve the game with the goal to get better at the projekt\
+      </p>\
+    </div>\
+    ');
