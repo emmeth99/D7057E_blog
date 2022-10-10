f0ef43b7962ac9a53209cdc7e75232f21574e5fb
emmeth <fantaexotic1999@gmail.com>
2022-10-10 10:43:43 +0200

    my emojis wont work :(((

--- frontEndBlog/10_10_22.js
+++ frontEndBlog/10_10_22.js
@@ -12,11 +12,11 @@ document.write(
             Sprint 1 is over and we have a working demo. Right now our group leader is holding a demo for Tromb \
             with the other group leaders to show our progress. Tomorrow we will show the demo to Peter and the class. \
             What we have right now is basically a front page with a leaderboard, 2 buttons and a temporary logo. The buttons leads \
-            to a page with a game board where you can actually play tetris and then there´s also some \
-            back end stuff done that I don´t really know anything about. It´s not perfect but it is a working demo :) \
+            to a page with a game board where you can actually play tetris and then there\'s also some \
+            back end stuff done that I don\'t really know anything about. It\'s not perfect but it is a working demo :) \
             <br> <br>\
-            Next up we will have a meeting within the front end group to plan and decide what we want to accomplish sprint 2.\
-            (Also note to ourselves -> please upload a blog post with a cool video of a demo soon, I´m looking at you Oskar &#128064;)\
+            Next up we will have a meeting within the front end group to plan and decide what we want to accomplish sprint 2. <br>\
+            (Also note to ourselves -> please upload a blog post with a cool video of a demo soon, I\'m looking at you Oskar &#128064;)\
         </p>\
     </div>\
     ');
