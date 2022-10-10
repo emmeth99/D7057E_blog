54f5b2f01787e6059c10538dcdd0226cb82847e9
Alex P <Alex.peschel@gmail.com>
2022-10-10 11:53:00 +0200

    add a blog post for the begning of sprint 2

---
+++ gameDevBlog/11_10_22.js
@@ -0,0 +1,25 @@
+document.write(
+    '<div class="post">\
+      <div style="display:flex;">\
+        <h2> Begining of Sprint 2 </h2>\
+          <div style="margin-left: auto;">\
+          <h3> GameDev </h3>\
+          <h3> 11/10-22 </h3>\
+        </div>\
+      </div>\
+    \
+      <p>\
+      This is the beginning of sprint 2 for gameDev. Here the goal is to break eveyrthing we have created and destroy it.\
+      <br><br>\
+      During sprint 1 we wanted to get a working prototype upp. The goal was to have a game which had the simple implementations\
+       which exists within the normal tetris. With rotations, with movement, with clear block. However, we aren\'t that happy \
+       with the code. We want to spend our time with refactorying. Break large methords into smaller and split functionalitys from \
+       places where there shouldn\'t be.\
+       <br><br>\
+      However, we have other plans for the sprint other then refactoring. We will create the store functions for the blocks, \
+      be able to se the score we have accuried. Then we also want to solve some bugs which currently exist within the code.\
+      Then most of all, add the functionality of the special tile block. This is so that we can have a block such as tromb \
+      giving speciall effects.\
+      </p>\
+    </div>\
+    ');
