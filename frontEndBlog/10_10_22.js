67160c490682f98b746e79c8212933be44741150
emmeth <fantaexotic1999@gmail.com>
2022-10-10 10:33:56 +0200

    added a frontend blog post

---
+++ frontEndBlog/10_10_22.js
@@ -0,0 +1,22 @@
+document.write(
+    '<div class="post">\
+        <div style="display:flex;">\
+            <h2> Sprint 1 is over! &#1F929 </h2>\
+            <div style="margin-left: auto;">\
+                <h3> FrontEnd </h3>\
+                <h3> 10/10-22 </h3>\
+            </div>\
+        </div>\
+    \
+        <p>\
+            Sprint 1 is over and we have a working demo. Right now our group leader is holding a demo for Tromb \
+            with the other group leaders to show our progress. Tomorrow we will show the demo to Peter and the class. \
+            What we have right now is basically a front page with a leaderboard, 2 buttons and a temporary logo. The buttons leads \
+            to a page with a game board where you can actually play tetris and then there´s also some \
+            back end stuff done that I don´t really know anything about. It´s not perfect but it is a working demo :) \
+            <br> <br>\
+            Next up we will have a meeting within the front end group to plan and decide what we want to accomplish sprint 2.\
+            (Also note to ourselves -> please upload a blog post with a cool video of a demo soon, I´m looking at you Oskar &#1F440)\
+        </p>\
+    </div>\
+    ');
