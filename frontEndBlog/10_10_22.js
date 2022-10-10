a7f97f22474e20bde0f79898e35faa85f6a63c47
emmeth <fantaexotic1999@gmail.com>
2022-10-10 10:35:48 +0200

    emoji fix I hope

--- frontEndBlog/10_10_22.js
+++ frontEndBlog/10_10_22.js
@@ -1,7 +1,7 @@
 document.write(
     '<div class="post">\
         <div style="display:flex;">\
-            <h2> Sprint 1 is over! &#1F929 </h2>\
+            <h2> Sprint 1 is over! &#129321 </h2>\
             <div style="margin-left: auto;">\
                 <h3> FrontEnd </h3>\
                 <h3> 10/10-22 </h3>\
@@ -16,7 +16,7 @@ document.write(
             back end stuff done that I don´t really know anything about. It´s not perfect but it is a working demo :) \
             <br> <br>\
             Next up we will have a meeting within the front end group to plan and decide what we want to accomplish sprint 2.\
-            (Also note to ourselves -> please upload a blog post with a cool video of a demo soon, I´m looking at you Oskar &#1F440)\
+            (Also note to ourselves -> please upload a blog post with a cool video of a demo soon, I´m looking at you Oskar &#128064)\
         </p>\
     </div>\
     ');
