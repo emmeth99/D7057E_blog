d63d12aa49befc20110b3b96a6df4a6bfc628c56
Cloud327 <73951252+Cloud327@users.noreply.github.com>
2022-10-31 20:07:35 +0100

    Create 31_10_22.js
    new blog post

---
+++ gameDevBlog/31_10_22.js
@@ -0,0 +1,25 @@
+document.write(
+    '<div class="post">\
+      <div style="display:flex;">\
+        <h2> End of sprint 2 and start of sprint 3 </h2>\
+          <div style="margin-left: auto;">\
+          <h3> GameDev </h3>\
+          <h3> 21/9-22 </h3>\
+        </div>\
+      </div>\
+    \
+      <p>\
+      During sprint 2 our team has done a lot of work refactoring the code. Turns out bad code does not get better when left alone :P, but nevertheless we persevered. \
+      Apart from refactoring, we also implemented important key features to the game logic in tetris. For instance now there is a way for frontend to see the upcoming \
+      blocks, a way of holding a block, having a way to see if the game has ended and much more.\
+      Also we have looked alot more on multiplayer this sprint and while we did not manage to finish it, we did build the groundwork for implementing it in sprint 3. \
+      Sprinkle in some bugfixing, some merge conflicts, and an exam period, then you pretty much have the entire sprint 2 in a nutshell. Overall it went pretty well \
+      especcially considering the exam period taking up alot of time for some group members. The most difficult thing during this sprint has probably thinking about \
+      how to best connect all of the different parts aswell as thinking about how to write good code instead of bad code. \
+      <br><br>\
+      But now we are starting sprint 3 and this time we actually have some sprint goals(we kind of forgot to write down sprint goals for sprint 2 other than just a \
+      mutual consensus about what we should do) and looking to implement multiplayer, do some more refactoring(yes, because we love refactoring), look at how we can deploy \
+      a minimum viable product onto a webbserver and lastly fixing bugs/fixing perfomance issues.
+      </p>\
+    </div>\
+    ');
