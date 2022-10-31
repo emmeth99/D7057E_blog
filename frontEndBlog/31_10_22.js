e78a7b48dc7701a9f7cedad3b510528dcd6343c2
tomatis55 <joskarkarlsson@gmail.com>
2022-10-31 23:52:58 +0100

    added new blogpost + new logo

---
+++ frontEndBlog/31_10_22.js
@@ -0,0 +1,27 @@
+document.write(
+    '<div class="post">\
+        <div style="display:flex;">\
+            <h2> Sprint 3 is coming! &#129321; </h2>\
+            <div style="margin-left: auto;">\
+                <h3> FrontEnd </h3>\
+                <h3> 31/10-22 </h3>\
+            </div>\
+        </div>\
+    \
+        <p>\
+            Whats this? Two new blogposts in a row after a long period of silence, I can feel a change in the wind, a new sprint must be coming. \
+            \
+            We\'re finally done with Sprint 2 in which we made great strides towards our final goals, I\'ve been adding a bunch of new features to the \
+            page with the single player game such as rendering of the upcoming and held blocks, some fun stuff like scores. The other frontliners have \
+            been working on cool stuff like leaderboards, sound and profile page stuff, fun! We\'ve also cooked up some new designs for some new pages \
+            that you might get to see in the future! As for the ever-looming multiplayer aspect of our game, we in the frontend group are mostly ready \
+            actually. We even have a working frame for a multiplayer page, now we just need to get some more information from the backend and we\'re all set! \
+            \
+            Today we also did some planning for sprint 3, held a super intense demo for Tromb, and got the super-insightful \"Snyggt! Jag tycker alla funkar &#128077\" \
+            feedback on our 9 different proposed logos…\
+            \
+            Oh, and here\'s that demo video Emma promised I would upload last time, oops &#128517!\
+            You\'ll have to imagine your own Korobeiniki music though.\
+        </p>\
+    </div>\
+    ');
